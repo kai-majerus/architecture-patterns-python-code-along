638bf127036ee852ef16e0b7aa9826e951fc5a43
kai-majerus <kaimajerus93@gmail.com>
2023-02-01 09:24:42 +0000

    chapter 6 tests working

--- src/allocation/service_layer/services.py
+++ src/allocation/service_layer/services.py
@@ -4,6 +4,7 @@ from typing import Optional
 
 from src.allocation.domain import model
 from src.allocation.adapters.repository import AbstractRepository
+from src.allocation.service_layer import unit_of_work
 
 
 class InvalidSku(Exception):
@@ -14,27 +15,26 @@ def is_valid_sku(sku, batches):
     return sku in {b.sku for b in batches}
 
 
-def allocate(line: model.OrderLine, repo: AbstractRepository, session) -> str:
-    batches = repo.list()
-    if not is_valid_sku(line.sku, batches):
-        raise InvalidSku(f"Invalid sku {line.sku}")
-    batchref = model.allocate(line, batches)
-    session.commit()
-    return batchref
+def allocate(
+    orderid: str, sku: str, qty: int, uow: unit_of_work.AbstractUnitOfWork
+) -> str:
+    line = model.OrderLine(orderid, sku, qty)
+    with uow:
+        batches = uow.batches.list()
+        if not is_valid_sku(line.sku, batches):
+            raise InvalidSku(f"Invalid sku {line.sku}")
+        batchref = model.allocate(line, batches)
+        uow.commit()
+        return batchref
 
 
 def add_batch(
-    ref: str, sku: str, qty: int, eta: Optional[date], repo: AbstractRepository, session
+    ref: str,
+    sku: str,
+    qty: int,
+    eta: Optional[date],
+    uow: unit_of_work.AbstractUnitOfWork,
 ):
-    batch = model.Batch(ref, sku, qty, eta)
-    repo.add(batch)
-    session.commit()
-
-
-def deallocate(line: model.OrderLine, repo: AbstractRepository, session):
-    for batch in repo.list():
-        if batch.sku == line.sku and line in batch._allocations:
-            batch.deallocate(line)
-            session.commit()
-            return
-    raise Exception("Batch not allocated")
+    with uow:
+        uow.batches.add(model.Batch(ref, sku, qty, eta))
+        uow.commit()
--- tests/unit/test_services.py
+++ tests/unit/test_services.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.allocation.service_layer import services
+from src.allocation.service_layer import services, unit_of_work
 from src.allocation.domain import model
 from src.allocation.adapters import repository
 
@@ -19,77 +19,42 @@ class FakeRepository(repository.AbstractRepository):
         return list(self._batches)
 
 
-class FakeSession:
-    committed = False
+class FakeUnitOfWork(unit_of_work.AbstractUnitOfWork):
+    def __init__(self):
+        self.batches = FakeRepository([])
+        self.committed = False
 
     def commit(self):
         self.committed = True
 
-
-def test_returns_allocation():
-    line = model.OrderLine("o1", "COMPLICATED-LAMP", 10)
-    batch = model.Batch("b1", "COMPLICATED-LAMP", 100, eta=None)
-    repo = FakeRepository([batch])
-
-    result = services.allocate(line, repo, FakeSession())
-    assert result == "b1"
+    def rollback(self):
+        pass
 
 
-def test_error_for_invalid_sku():
-    line = model.OrderLine("o1", "NONEXISTENTSKU", 10)
-    batch = model.Batch("b1", "AREALSKU", 100, eta=None)
-    repo = FakeRepository([batch])
-
-    with pytest.raises(services.InvalidSku, match="Invalid sku NONEXISTENTSKU"):
-        services.allocate(line, repo, FakeSession())
-
-
-def test_commits():
-    line = model.OrderLine("o1", "OMINOUS-MIRROR", 10)
-    batch = model.Batch("b1", "OMINOUS-MIRROR", 100, eta=None)
-    repo = FakeRepository([batch])
-    session = FakeSession()
-    services.allocate(line, repo, session)
-    assert session.committed is True
+def test_allocate_returns_allocation():
+    uow = FakeUnitOfWork()
+    services.add_batch("batch1", "COMPLICATED-LAMP", 100, None, uow)
+    result = services.allocate("o1", "COMPLICATED-LAMP", 10, uow)
+    assert result == "batch1"
 
 
 def test_add_batch():
-    repo, session = FakeRepository([]), FakeSession()
-    services.add_batch("b1", "CRUNCHY-ARMCHAIR", 100, None, repo, session)
-    assert repo.get("b1") is not None
-    assert session.committed
+    uow = FakeUnitOfWork()
+    services.add_batch("b1", "CRUNCHY-ARMCHAIR", 100, None, uow)
+    assert uow.batches.get("b1") is not None
+    assert uow.committed
 
 
-def test_deallocate_decrements_available_quantity():
-    repo, session = FakeRepository([]), FakeSession()
-    services.add_batch("b1", "BLUE-PLINTH", 100, None, repo, session)
-    line = model.OrderLine("o1", "BLUE-PLINTH", 10)
-    services.allocate(line, repo, session)
-    batch = repo.get(reference="b1")
-    assert batch.available_quantity == 90
-    services.deallocate(line, repo, session)
-    assert batch.available_quantity == 100
+def test_allocate_errors_for_invalid_sku():
+    uow = FakeUnitOfWork()
+    services.add_batch("b1", "AREALSKU", 100, None, uow)
+
+    with pytest.raises(services.InvalidSku, match="Invalid sku NONEXISTENTSKU"):
+        services.allocate("o1", "NONEXISTENTSKU", 10, uow)
 
 
-def test_deallocate_decrements_correct_quantity():
-    repo, session = FakeRepository([]), FakeSession()
-    services.add_batch("b1", "BLUE-PLINTH", 100, None, repo, session)
-    services.add_batch("b2", "RED-PLINTH", 200, None, repo, session)
-    line = model.OrderLine("o1", "BLUE-PLINTH", 10)
-    services.allocate(line, repo, session)
-    blue_batch = repo.get(reference="b1")
-    red_batch = repo.get(reference="b2")
-    assert blue_batch.available_quantity == 90
-    assert red_batch.available_quantity == 200
-    services.deallocate(line, repo, session)
-    assert blue_batch.available_quantity == 100
-    assert red_batch.available_quantity == 200
-
-
-def test_trying_to_deallocate_unallocated_batch():
-    repo, session = FakeRepository([]), FakeSession()
-    services.add_batch("b1", "BLUE-PLINTH", 100, None, repo, session)
-    line = model.OrderLine("o1", "BLUE-PLINTH", 10)
-
-    with pytest.raises(Exception, match="Batch not allocated"):
-        services.deallocate(line, repo, session)
+def test_allocate_commits():
+    uow = FakeUnitOfWork()
+    services.add_batch("b1", "OMINOUS-MIRROR", 100, None, uow)
+    services.allocate("o1", "OMINOUS-MIRROR", 10, uow)
+    assert uow.committed
